6bb838785d0ea335c23108764a02eb28751b9de4
Nimira43 <n.rabone72@aol.com>
2025-11-29 23:50:44 +0000

    Created 3 new flexbox custom CSS classes.

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className='flex items-center justify-center h-screen'>
+    <div className=' h-screen'>
       <div className='w-20 h-20 border-t-2 border-b-2 border-main rounded-full animate-spin'></div>
     </div>
   )
