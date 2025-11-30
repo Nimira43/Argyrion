1053fe73cb87f025a842406d23b8ce07a041e876
Nimira43 <n.rabone72@aol.com>
2025-11-30 00:10:30 +0000

    Creating ProductCardSkeleton.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -7,7 +7,7 @@ export default function Loading() {
       <h1 className='text-3xl mb-6 text-medium'>Home</h1>
       <p>Showing some products</p>
       <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
-        {Array.from({length: 6}).map((_, i) => (
+        {Array.from({length: 9}).map((_, i) => (
           <div key={i}>Placeholder</div>
         ))}
       </div>
