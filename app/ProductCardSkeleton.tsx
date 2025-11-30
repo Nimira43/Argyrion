fe9b3d4963b8e06ca6f9dafefbd464bf6e8ce5e3
Nimira43 <n.rabone72@aol.com>
2025-11-30 00:11:58 +0000

    Copying all code from ProductCard into ProductCardSkeleton

---
+++ app/ProductCardSkeleton.tsx
@@ -0,0 +1,33 @@
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { formatPrice } from '@/lib/utils'
+import { Product } from '@prisma/client'
+import Image from 'next/image'
+
+export function ProductCard({
+  product
+}: {
+  product: Product
+}) {
+  return (
+    <Card className='pt-0 overflow-hidden'>
+      <div className='relative aspect-video'>
+        {product.image && (
+          <Image
+            src={product.image} 
+            alt={`Image of ${product.name}`}
+            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+            fill
+            className='object-cover'
+          />
+        )}
+      </div>
+      <CardHeader>
+        <CardTitle>{product.name}</CardTitle>
+        <CardDescription>{product.description}</CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <p>{formatPrice(product.price)}</p>
+      </CardFooter>
+    </Card>
+  )
+}
