28426e20ce2006002272ade705b7ec5dcaa7de7c
Nimira43 <n.rabone72@aol.com>
2025-11-30 00:08:58 +0000

    Creating temporary loader.

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,13 +5,10 @@ export default function Loading() {
     // </div>
     <main className='container mx-auto p-4'>
       <h1 className='text-3xl mb-6 text-medium'>Home</h1>
-      <p>Showing {products.length} products</p>
+      <p>Showing some products</p>
       <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
-        {products.map((product) => (
-          <ProductCard 
-            key={product.id}
-            product={product}
-          />
+        {Array.from({length: 6}).map((_, i) => (
+          <div key={i}>Placeholder</div>
         ))}
       </div>
     </main>
