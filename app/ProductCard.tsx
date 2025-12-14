37f78fc2a6e17da3d9facda04ee7d0191165dafc
Nimira43 <n.rabone72@aol.com>
2025-12-14 02:15:36 +0000

    Wrapped ProductCard content inside a Link to ProductPage.

--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/comp
 import { formatPrice } from '@/lib/utils'
 import { Product } from '@prisma/client'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function ProductCard({
   product
@@ -9,25 +10,27 @@ export function ProductCard({
   product: Product
 }) {
   return (
-    <Card className='mt-6 pt-0 overflow-hidden'>
-      <div className='relative aspect-video'>
-        {product.image && (
-          <Image
-            src={product.image} 
-            alt={`Image of ${product.name}`}
-            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-            fill
-            className='object-cover'
-          />
-        )}
-      </div>
-      <CardHeader>
-        <CardTitle>{product.name}</CardTitle>
-        <CardDescription>{product.description}</CardDescription>
-      </CardHeader>
-      <CardFooter>
-        <p>{formatPrice(product.price)}</p>
-      </CardFooter>
-    </Card>
+    <Link href={`/product/${product.slug}`}>
+      <Card className='mt-6 pt-0 overflow-hidden'>
+        <div className='relative aspect-video'>
+          {product.image && (
+            <Image
+              src={product.image} 
+              alt={`Image of ${product.name}`}
+              sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+              fill
+              className='object-cover'
+            />
+          )}
+        </div>
+        <CardHeader>
+          <CardTitle>{product.name}</CardTitle>
+          <CardDescription>{product.description}</CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <p>{formatPrice(product.price)}</p>
+        </CardFooter>
+      </Card>
+    </Link>
   )
 }
