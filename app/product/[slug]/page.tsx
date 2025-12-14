abd456e55869bd5ab385b6282249905d477d7774
Nimira43 <n.rabone72@aol.com>
2025-12-14 01:47:32 +0000

    ProductPage - 3

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,7 +1,9 @@
+import { getProductBySlug } from '@/lib/actions'
+
 export async function ProductPage({
   params
 }: {
   params: { slug: string }
 }) {
-  
+  const product = await getProductBySlug(params.slug)
 }
