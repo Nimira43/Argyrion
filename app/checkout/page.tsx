34e69583ac800bbedd161dce5561ffa880526685
Nimira43 <n.rabone72@aol.com>
2026-07-24 00:48:57 +0100

    Drawing this project to a close - decent frontend design and data served via Postgres Docker container. Project concluded.

---
+++ app/checkout/page.tsx
@@ -0,0 +1,5 @@
+export default function CheckoutPage() {
+  return (
+    <div className='m-20'>Argyrion project has concluded - frontend with cart functionality and data served via a Postgres database use a Docker container.</div>
+  )
+}
